cc4536f3cd4b6ecdc10a8ca0a82052d49be34c98
GSalgadoLC <98500704+GSalgadoLC@users.noreply.github.com>
2022-08-16 12:30:37 -0700

    Add files via upload

--- string_reversal.py
+++ string_reversal.py
@@ -1,12 +1,19 @@
-str_to_reverse = "Limes are green"
-iteration = len(str_to_reverse)-1
+# str[start:stop:step]
 
-for letter in str_to_reverse:
-    print(str_to_reverse[iteration])
-    iteration -= 1
+trial = "reversal"
+new_trial = trial[::-1]
+print(new_trial)
 
-# This is my first though on how I would perform string reversal.
-# I first find how many letters/characters are in the string and subtract 1
-# We can now use to index the string without going out of the range
-# Now we use a for loop and start with the last index of the string and print
-# We subtract 1 until we have printed the entire string
+
+# Recursion method
+
+def string_reversal(str):
+    if len(str) == 0:
+        return str
+    else:
+        return string_reversal(str[1:]) + str[0]
+
+
+str = "reversal"
+reverse = string_reversal(str)
+print(reverse)
